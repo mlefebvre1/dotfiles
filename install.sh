e2866f572cb62151b592564ff05d3ea299dce548
Michael <michael.lefebvre@riedel.net>
2021-11-08 10:51:30 -0500

    bring back zeta theme

--- install.sh
+++ install.sh
@@ -8,3 +8,6 @@ cp .zsh_functions ~/.zsh_functions
 # get zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+#change zsh theme to zeta
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/skylerlee/zeta-zsh-theme/master/scripts/install.sh)"
