e43913fb93d87f0fc4371149041a6d4ebb137721
Michael <michael.lefebvre@riedel.net>
2024-06-21 14:03:17 -0400

    add some .config files

--- install.sh
+++ install.sh
@@ -9,7 +9,9 @@ cp .zshrc ~/.zshrc
 # get zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-
 git clone https://github.com/cdimascio/lambda-zsh-theme.git && cp lambda-zsh-theme/cdimascio-lambda.zsh-theme ~/.oh-my-zsh/custom/themes
+
+# install .config files
+cp -r .config/ ~./config
 
 exit 0
